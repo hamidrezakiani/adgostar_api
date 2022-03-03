08055c6fa2c695cffab210a3058207dbcf8b2dd7
hamidrezakiani <53411134+hamidrezakiani@users.noreply.github.com>
2022-03-03 23:11:55 +0330

    add log function

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -3,5 +3,11 @@
 namespace App\Helper;
 
 class Helper { 
-
-}
+  
+   public function log($data)
+   {
+    $fp = fopen(url('log.json'), 'w');
+    fwrite($fp, json_encode($data));
+    fclose($fp);
+   };
+};
