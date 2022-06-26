2820a027ebb50bdc17daa099cf58efa5b21ea2ff
hamidrezakiani <hamidboresi@gmail.com>
2022-06-26 13:35:45 +0430

    sql speed test

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -48,7 +48,7 @@ class StudentController extends Controller
      */
     public function update(Request $request,$count)
     {
-        $servername = 'localhost:3306';
+        $servername = 'http://localhost:3306';
         $username = env('DB_USERNAME');
         $password = env('DB_PASSWORD');
         $dbname = "itdevir_test";
